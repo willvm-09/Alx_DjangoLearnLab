666cc29254ed3aa0a1e9b220d0c19c3ab1fe35af
willvm-09 <williammutugi7@gmail.com>
2024-08-20 17:57:57 +0300

    Update class name to match checker

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import list_books, LibraryView
+from .views import list_books, LibraryDetailView
 
 urlpatterns = [
     path('relationship_app/', list_books, name='list_books'),
-    path('relationship_app/', LibraryView.as_view(), name='library_detail')
+    path('relationship_app/', LibraryDetailView.as_view(), name='library_detail')
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -10,7 +10,7 @@ def list_books(request):
     context = {'list_books': books}
     return render(request, 'relationship_app/list_books.html', context)
 
-class LibraryView(DetailView):
+class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
