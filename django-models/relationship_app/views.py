71f0ba3b805fbe02a270d7f55b599b4294866395
willvm-09 <williammutugi7@gmail.com>
2024-08-20 18:17:17 +0300

    change path

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -8,16 +8,16 @@ from .models import Book
 def list_books(request):
     books = Book.objects.all()
     context = {'list_books': books}
-    return render(request, 'templates/list_books.html', context)
+    return render(request, 'relationship_app/list_books.html', context)
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'templates/library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['relationship_app'] = self.books.all()
+        context['books'] = self.books.all()
         return context
 
 
