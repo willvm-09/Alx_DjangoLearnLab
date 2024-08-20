a8ea7075dc6e40596bbacf4065ec2e28261e6159
willvm-09 <williammutugi7@gmail.com>
2024-08-20 17:52:24 +0300

    Change path

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import list_books, LibraryView
 
 urlpatterns = [
-    path('books/', list_books, name='list_books'),
-    path('library/<int:pk>/', LibraryView.as_view(), name='library_detail')
+    path('relationship_app/', list_books, name='list_books'),
+    path('relationship_app/', LibraryView.as_view(), name='library_detail')
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -7,16 +7,16 @@ from .models import Book, Library
 def list_books(request):
     books = Book.objects.all()
     context = {'list_books': books}
-    return render(request, 'books/list_books.html', context)
+    return render(request, 'relationship_app/list_books.html', context)
 
 class LibraryView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['books'] = self.books.all()
+        context['relationship_app'] = self.books.all()
         return context
 
 
