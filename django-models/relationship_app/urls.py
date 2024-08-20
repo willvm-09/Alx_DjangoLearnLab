a7fea8de038f1757350257702e4dc81625c379d9
willvm-09 <williammutugi7@gmail.com>
2024-08-20 21:55:56 +0300

    Update path

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -6,8 +6,8 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('relationship_app/', views.list_books, name='list_books'),
     path('relationship_app/', views.LibraryDetailView.as_view(), name='library_detail'),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name=""), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name=""), name='logout'),
     path('register/', views.register, name='register'),
 ]
     
