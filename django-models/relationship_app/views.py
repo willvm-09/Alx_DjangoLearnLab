ea239a9749b5c9056b135388777e9a5535da468f
willvm-09 <williammutugi7@gmail.com>
2024-08-21 13:36:00 +0300

    Amend path

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -54,12 +54,12 @@ def is_member(user):
 
 @user_passes_test(is_admin)
 def admin_view(request):
-    return render(request, 'admin_view.html')
+    return render(request, 'relationship_app/admin_view.html')
 
 @user_passes_test(is_librarian)
 def librarian_view(request):
-    return render(request, 'librarian_view.html')
+    return render(request, 'relationship_app/librarian_view.html')
 
 @user_passes_test(is_member)
 def member_view(request):
-    return render(request,'member_view.html')
+    return render(request,'relationship_app/member_view.html')
