f723ecae581f2b279abadec44c26aac18d6b4fe5
willvm-09 <williammutugi7@gmail.com>
2024-08-21 13:07:44 +0300

    Update code

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -40,13 +40,13 @@ from django.contrib.auth.decorators import user_passes_test
 from .models import UserProfile
 
 def is_admin(user):
-    return user.is_authenticated and user.userprofile.role == 'ADMIN'
+    return user.is_authenticated and user.userprofile.role == 'Admin'
 
 def is_librarian(user):
-    return user.userprofile.role == 'LIBRARIAN'
+    return user.userprofile.role == 'Librarian'
 
 def is_member(user):
-    return user.userprofile.role == 'MEMBER'
+    return user.userprofile.role == 'Member'
 
 @user_passes_test(is_admin)
 def admin_view(request):
@@ -136,12 +136,6 @@ def delete_book(request, pk):
         book.delete()
         return redirect('library_detail', pk=book.library.pk)
     return render(request, 'relationship_app/delete_book.html', {'book': book})
-
-
-
-
-
-
 
 
 from typing import Any
