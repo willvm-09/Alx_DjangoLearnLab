22a6b232e6d1d62e7762d1fbd200f5086ed30789
willvm-09 <williammutugi7@gmail.com>
2024-09-06 17:44:59 +0300

    Change import to match checker

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics, filters
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 # Create your views here.
 
@@ -10,7 +10,7 @@ class ListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
-    
+
 
 class DetailView(generics.RetrieveView):
     queryset = Book.objects.all()
