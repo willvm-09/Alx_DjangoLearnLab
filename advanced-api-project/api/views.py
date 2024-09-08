d968f81fc85ac646ec14d0a9bc3f00edd6a4c8c6
willvm-09 <williammutugi7@gmail.com>
2024-09-08 14:22:09 +0300

    filtering, searching, and ordering in REST

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from rest_framework import generics, filters
+from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
 
 # Create your views here.
 
@@ -10,12 +13,21 @@ class ListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = BookSerializer
+
+    filter_backends = [OrderingFilter]
+    filterset_class = BookSerializer
+    ordering_fields = ['title', 'publication_year']
+    ordering = ['title']
 
 
 class DetailView(generics.RetrieveView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'author', 'publication_year']
 
 class CreateView(generics.CreateAPIView): 
     queryset = Book.objects.all()
@@ -26,6 +38,7 @@ class UpdateView(generics.UpdateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticated]
+    
 
 
 class DeleteView(generics.DestroyAPIView):
