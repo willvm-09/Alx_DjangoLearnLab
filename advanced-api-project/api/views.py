ff5846e49f007e90b795d9a9528e82f01f4a3c1d
willvm-09 <williammutugi7@gmail.com>
2024-09-08 14:23:29 +0300

    filtering, searching, and ordering in REST

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from django_filters import rest_framework
 from rest_framework import filters
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter
