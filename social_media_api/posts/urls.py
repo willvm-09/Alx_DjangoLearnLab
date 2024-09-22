1703cf1dc9f68442aa887de236a21151ca72756d
willvm-09 <williammutugi7@gmail.com>
2024-09-22 12:06:08 +0300

    update url path

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -9,6 +9,6 @@ router.register(r'comments', CommentViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('feed/', UserFeedView.as_view(), name='user-feed'),
-    path('like/<int:post_id>/', LikePostView.as_view(), name='like-post'),
-    path('unlike/<int:post_id>/', UnlikePostView.as_view(), name='unlike-post'),
+    path('<int:pk>/like/', LikePostView.as_view(), name='like-post'),
+    path('<int:pk>/unlike/', UnlikePostView.as_view(), name='unlike-post'),
 ]
